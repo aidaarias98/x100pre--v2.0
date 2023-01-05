62abdf5b8549bec3f65279325400225a6a124fd6
R2H <r2h@AdminisatorsMBP.attlocal.net>
2023-01-05 00:31:16 -0500

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './pages/Homepage/Home'
+import About from './pages/About/About'
+import Products from './pages/Products/Products'
+import Contact from './pages/Contact/Contact'
+import Navigation from './components/NavBar/Navigation'
+import Footer from  './components/Footer/Footer'
+import './App.css'
+
+
+function App() {
+  return (
+    <div className='App'>
+      
+    <Router>
+    <Navigation/>
+    <br/>
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/aboutus' element={<About/>}/>
+    <Route path='/products' element={<Products/>}/>
+    <Route path='/contactus' element={<Contact/>}/>
+    </Routes>
+    </Router>
+    <Footer/>
+    
+    </div>
+  )
+}
+
+export default App
